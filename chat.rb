6cd2b68f32ef208c4413d16d2032bebcc2256b07
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-04 19:45:10 +0000

    Updated to 1001 Arabian Nights Narrator

--- chat.rb
+++ chat.rb
@@ -1,18 +1,29 @@
-ai-chat-1 main % ruby chat.rb
-chat.rb: --> chat.rb
-Unmatched keyword, missing `end' ?
-   1  require "bundler/setup"
-   2  require "ai-chat"
-   3  require "dotenv/load"
-   7  c = AI::Chat.new
-   9  c.system("You are a helpful assistant who talks like Shakespeare.")
-  11  c.user("Hello! What are the best spots for pizza in Chicago?")
-  13  ai_reply = c.generate!
-  15  pp ai_reply
-  17  ap ai_reply
-  19  ap c
-  21  ap c.messages
-> 23  while user_input != "bye"
-chat.rb:31: syntax error, unexpected end-of-input (SyntaxError)
-  end
-     ^
+require "ai-chat"
+require "dotenv/load"
+require "amazing_print"
+
+c = AI::Chat.new
+c.model = "o4-mini"
+
+c.add("You are the narrator of 1001 Arabian Nights", role: "system")
+
+puts "Welcome! Please wait a moment while I create an adventure just for you...."
+
+user_prompt = nil
+
+while user_prompt != "exit"
+  puts
+  puts "-"*25
+  puts
+  
+  puts c.generate!
+  
+  puts
+  puts "-"*25
+  puts
+  
+  print ">> "
+  user_prompt = gets.chomp
+  
+  c.add(user_prompt)
+end
