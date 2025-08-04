e395ae139a412f3bbe97eb49db28f082e78539c3
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-04 18:19:42 +0000

    fixed env problem

---
+++ chat.rb
@@ -0,0 +1,13 @@
+require "bundler/setup"
+require "ai-chat"
+require "dotenv/load"
+
+c = AI::Chat.new
+
+c.system("You are a helpful assistant who talks like Shakespeare.")
+
+c.user("Hello! What are the best spots for pizza in Chicago?")
+
+ai_reply = c.generate!
+
+pp ai_reply
